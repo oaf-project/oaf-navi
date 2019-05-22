793feb97c0cc20c80398dc12ade705297f41bd98
Daniel Nixon <dan.nixon@gmail.com>
2019-05-22 17:41:21 +1000

    Update oaf-routing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { History } from "history";
+import { History, LocationKey } from "history";
 import { Navigation, Route } from "navi";
 import {
   createOafRouter,
@@ -18,6 +18,11 @@ export const defaultSettings: RouterSettings<Route<unknown>> = {
       ? route.title
       : oafRoutingDefaultSettings.documentTitle(route),
 };
+
+// HACK we need a way to track where focus and scroll were left on the first loaded page
+// but we won't have an entry in history for this initial page, so we just make up a key.
+const orInitialKey = (key: LocationKey | undefined): LocationKey =>
+  key !== undefined ? key : "initial";
 
 export const wrapNavigation = async <
   LocationState = unknown,
@@ -51,7 +56,7 @@ export const wrapNavigation = async <
       oafRouter.handleLocationChanged(
         previousRoute,
         route,
-        history.location.key,
+        orInitialKey(history.location.key),
         history.action,
       );
 
@@ -63,8 +68,8 @@ export const wrapNavigation = async <
   // See https://github.com/ReactTraining/history/issues/690
   const unblock = history.block((location, action) => {
     oafRouter.handleLocationWillChange(
-      history.location.key,
-      location.key,
+      orInitialKey(history.location.key),
+      orInitialKey(location.key),
       action,
     );
   });
